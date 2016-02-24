a8d80423ba2d5eaec66f4bc9d95787a1d6eb2137
Simon Gustafsson <simong@simong.se>
2016-02-24 23:57:00 +0100

    Increased throughput to 18 reading per second
    At this throughput, we read out the same value multiple times because the
    DMM is updating its reading at most around 7 times a second in most modes,
    or up to 14 times a second in ohms and diode mode.

--- u1272a_live_logger.py
+++ u1272a_live_logger.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import serial
+import sys
 import time
 from datetime import datetime;
 
 ser = serial.Serial('/dev/ttyUSB0', timeout=0.5)
 
 def send_receive(command):
+	if ser.inWaiting():
+		#Providing a way to resynchronize, if we have several outstanding requests
+		#(only seen this kick in when the user rotates the range switch)
+		print("WARNING: Throwing away \"" + ser.read(1000) + "\"", file=sys.stderr)
+
 	ser.write(command + '\n')
 	time.sleep(0.02)
-	received = ser.read(100)
+	received = ser.readline()
 	received = received.replace('\n','')
 	received = received.replace('\r','')
 	return received
@@ -26,8 +34,10 @@ def send_receive(command):
 #send_receive('READ?')
 
 idn = send_receive('*IDN?')
-print '# Measurement source: %s\n' %idn
-print 'Timestamp\tReading\tRange setting'
+print('# Measurement source: %s\n'% idn)
+print('Counter\tTimestamp\tReading\tRange setting')
+
+n=0
 while True:
 	reading = send_receive('READ?')
 	conf = send_receive('CONF?')
@@ -36,4 +46,5 @@ while True:
 	usecs = now.microsecond
 	timestring = now.strftime("%Y-%m-%d %H:%M:%S") + '.%03d' % (usecs/1000)
 
-	print '%s\t%s\t%s' %(timestring, reading, conf)
+	print('%d\t%s\t%s\t%s'% (n, timestring, reading, conf))
+	n += 1
